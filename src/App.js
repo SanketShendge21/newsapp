0c594bc2f723e89294e8b4c652a747a915dd81ac
SanketShendge21 <sanketshendge21052002@gmail.com>
2023-10-27 18:35:31 +0530

    Converted complete app into Functional-based Componenets

--- src/App.js
+++ src/App.js
@@ -11,22 +11,20 @@ import {
 
 import LoadingBar from 'react-top-loading-bar';
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class App extends Component {
-  pageSize = 15
+export default function App() {
+  let pageSize = 15
 
-  apiKey = process.env.REACT_APP_NEWS_API
+  let apiKey = process.env.REACT_APP_NEWS_API
 
-  state = {
-    progress : 0
-  }
+  const [progress,setProgress] = useState(0)
 
-  setProgress = (progress)=>{
-    this.setState({progress : progress})
-  }
+  // const setProgress = (progress)=>{
+  //   setState({progress : progress})
+  // }
 
-  render() {
+
     return (
       <div>
         <Router>
@@ -34,26 +32,26 @@ export default class App extends Component {
         <LoadingBar
         height={3}
         color='#f11946'
-        progress={this.state.progress}
-        onLoaderFinished={() => this.setProgress(0)}
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
       />
 
         <Navbar></Navbar>
 
     
         <Routes>
-          <Route path='/' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="general"></News>}></Route>
+          <Route path='/' element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"></News>}></Route>
 
-          <Route exact path='/sports' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="sports"></News>}></Route>
-          <Route exact path='/business' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="business"></News>}></Route>
-          <Route exact path='/science' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="science"></News>}></Route>
-          <Route exact path='/health' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="health"></News>}></Route>
-          <Route exact path='/entertainment' element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="entertainment"></News>}></Route>
+          <Route exact path='/sports' element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"></News>}></Route>
+          <Route exact path='/business' element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"></News>}></Route>
+          <Route exact path='/science' element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"></News>}></Route>
+          <Route exact path='/health' element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"></News>}></Route>
+          <Route exact path='/entertainment' element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"></News>}></Route>
         </Routes>
         </Router>
       </div>
     )
-  }
+  
 }
 
 
--- src/components/News.js
+++ src/components/News.js
@@ -1,85 +1,72 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from 'react-infinite-scroll-component';
-export class News extends Component {
+const News = (props) => {
 
-  static defaultProps={
-    country : 'in',
-    pageSize : 8,
-    category : 'general'
+  const [articles,setArticles] = useState([])
+  const [loading,setLoading] = useState(true)
+  const [page,setPage] = useState(1)
+  const [totalResults,setTotalResults] = useState(0)
+
+  
+  const capitalizeFirstLetter = (string) =>{
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  static propTypes = {
-    country : PropTypes.string,
-    pageSize : PropTypes.number,
-    category : PropTypes.string
-  }
+  document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`
 
-  articles = [
-    {
-    "source": {
-    "id": null,
-    "name": "BBC News"
-    },
-    "author": "https://www.facebook.com/bbcnews",
-    "title": "Drug donanemab seen as turning point in dementia fight - BBC",
-    "description": "The antibody treatment donanemab helps clear the brain of a damaging protein that builds up in Alzheimer's.",
-    "url": "https://www.bbc.com/news/health-66221116",
-    "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/598B/production/_127832922_brain_scans_woman_getty.jpg",
-    "publishedAt": "2023-07-17T14:16:16Z",
-    "content": "Results out today confirm that the drug donanemab, hailed as a turning point in the fight against Alzheimer's, slows cognitive decline by about a third.\r\nMike Colley, who is 80, is one of only a few … [+5975 chars]"
-    },
-    {
-    "source": {
-    "id": null,
-    "name": "MarketWatch"
-    },
-    "author": "Quentin Fottrell",
-    "title": "The Moneyist answers: Should I tip before or after tax and credit-card charges? - MarketWatch",
-    "description": "‘I won’t tip at a deli counter, but I will tip more in a diner,’ one reader says.",
-    "url": "https://www.marketwatch.com/story/tipping-recommendations-in-restaurants-are-based-on-faulty-calculations-should-i-tip-before-or-after-tax-and-credit-card-charges-eabff2a5",
-    "urlToImage": "https://images.mktw.net/im-262198/social",
-    "publishedAt": "2023-07-17T14:10:00Z",
-    "content": "Ive read your previous responses to letters on tipping, and my thoughts are simple: Tipping is dependent on the service given. I wont tip at a deli counter, but I will tip more in a diner. I see no r… [+5729 chars]"
-    }
-    ];
+  // articles = [
+  //   {
+  //   "source": {
+  //   "id": null,
+  //   "name": "BBC News"
+  //   },
+  //   "author": "https://www.facebook.com/bbcnews",
+  //   "title": "Drug donanemab seen as turning point in dementia fight - BBC",
+  //   "description": "The antibody treatment donanemab helps clear the brain of a damaging protein that builds up in Alzheimer's.",
+  //   "url": "https://www.bbc.com/news/health-66221116",
+  //   "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/598B/production/_127832922_brain_scans_woman_getty.jpg",
+  //   "publishedAt": "2023-07-17T14:16:16Z",
+  //   "content": "Results out today confirm that the drug donanemab, hailed as a turning point in the fight against Alzheimer's, slows cognitive decline by about a third.\r\nMike Colley, who is 80, is one of only a few … [+5975 chars]"
+  //   },
+  //   {
+  //   "source": {
+  //   "id": null,
+  //   "name": "MarketWatch"
+  //   },
+  //   "author": "Quentin Fottrell",
+  //   "title": "The Moneyist answers: Should I tip before or after tax and credit-card charges? - MarketWatch",
+  //   "description": "‘I won’t tip at a deli counter, but I will tip more in a diner,’ one reader says.",
+  //   "url": "https://www.marketwatch.com/story/tipping-recommendations-in-restaurants-are-based-on-faulty-calculations-should-i-tip-before-or-after-tax-and-credit-card-charges-eabff2a5",
+  //   "urlToImage": "https://images.mktw.net/im-262198/social",
+  //   "publishedAt": "2023-07-17T14:10:00Z",
+  //   "content": "Ive read your previous responses to letters on tipping, and my thoughts are simple: Tipping is dependent on the service given. I wont tip at a deli counter, but I will tip more in a diner. I see no r… [+5729 chars]"
+  //   }
+  //   ];
 
-    capitalizeFirstLetter = (string) =>{
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    }
-  constructor(props){
-    
-    super(props);
-    // console.log("Hello im a constructor from news component");
-    this.state = {
-      articles: [],
-      loading: false,
-      page : 1,
-      totalResults: 0
-    }
-    document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
-  }
+
  
-  async updateNews(){
-    this.props.setProgress(10)
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`;
-      this.setState({loading : true})
+  const updateNews = async () => {
+    props.setProgress(10)
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
+      // this.setState({loading : true})
+      setLoading(true)
       let data = await fetch(url);
       let parsedData = await data.json()
       // console.log(parsedData);
-      this.setState({
-        articles: parsedData.articles,
-        totalResults: parsedData.totalResults,
-        loading : false
-      })
-      this.props.setProgress(100)
+      setArticles(parsedData.articles)
+      setTotalResults(parsedData.totalResults)
+      setLoading(false)
+
+      props.setProgress(100)
   }
 
-    async componentDidMount(){
-      this.updateNews()
-    }
+    useEffect(() => {
+      updateNews();
+    });
+
 
     // handlePrevClick = async ()=>{
     //   this.setState({page : this.state.page - 1});
@@ -91,39 +78,43 @@ export class News extends Component {
     //   this.updateNews();
     // }
     
-    fetchMoreData = async () => {
-      this.setState({page : this.state.page + 1})
-      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+    const fetchMoreData = async () => {
+      // this.setState({page : this.state.page + 1})
+      setPage(page + 1)
+      const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
 
       let data = await fetch(url);
       let parsedData = await data.json()
       // console.log(parsedData);
-      this.setState({
-        articles: this.state.articles.concat(parsedData.articles),
-        totalResults: parsedData.totalResults,
-        loading : false
-      })
+      // this.setState({
+      //   articles: this.state.articles.concat(parsedData.articles),
+      //   totalResults: parsedData.totalResults,
+      //   loading : false
+      // })
+      setArticles(articles.concat(parsedData.articles))
+      setTotalResults(parsedData.totalResults)
+      setLoading(false)
     };
  
 
-  render() {
+
     return (
 <>
-          <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+          <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
 
-          {this.state.loading && <Spinner />} {/* this says if loading true then show spinner component else not */}
+          {loading && <Spinner />} {/* this says if loading true then show spinner component else not */}
 
           <InfiniteScroll
-          dataLength={this.state.articles.length} // articles length to show 
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length < this.state.totalResults}
+          dataLength={articles.length} // articles length to show 
+          next={fetchMoreData}
+          hasMore={articles.length < totalResults}
           loader={<Spinner />}
         >
 
         {/* iterating over two elements as my articles contains only two objects */}
           <div className="container">
           <div className='row'>
-          {this.state.articles.map((element)=>{
+          {articles.map((element)=>{
             return  <div className="col-md-4" key={element.url}>
             <NewsItem  title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,90):""}
             imageUrl={!element.urlToImage?"https://images.macrumors.com/t/jHD1aGfXq86Ln_kfzTyWIF68vOo=/2368x/article-new/2021/02/Safari-Technology-Preview-Feature.jpg":element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}></NewsItem>
@@ -141,7 +132,17 @@ export class News extends Component {
      
       </>
     )
-  }
+  
+}
+News.defaultProps={
+  country : 'in',
+  pageSize : 8,
+  category : 'general'
 }
 
+News.propTypes = {
+  country : PropTypes.string,
+  pageSize : PropTypes.number,
+  category : PropTypes.string
+}
 export default News
